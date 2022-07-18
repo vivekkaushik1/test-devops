fe239dc292f3b425653e38591f7619388c7ead57
vivekkaushik1 <vivekit388@gmail.com>
2022-07-18 15:40:01 +0530

    dd

--- createCOmmitsTags.sh
+++ createCOmmitsTags.sh
@@ -1,14 +1,14 @@
 string=""
 x=1
 j=1
-while [ $x -le 10000 ]
+while [ $x -le 10 ]
 do
  rm -rf random*
  string="touch randomfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
  eval "$string"
  x=$[$x+1]
- sleep 7.0
+ sleep 1.0
  git push
  git tag -a Pt7.$((j+x)) -m "vversion Pt1.1$((j+x))"
  git push --tags origin
